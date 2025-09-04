f340dbe2a76ef6236f6b39bdd324e6020e2c2050
aandree12 <gabdushov3000@mail.ru>
2025-09-04 17:23:20 +0300

    roll

--- core.js
+++ core.js
@@ -4,3 +4,31 @@ $(".bouqets-link").on('click', '[href*="#our-anchor"]', function(e){
     e.preventDefault();
     
   });
+
+$(document).ready(function() {
+  // Закрываем меню при клике на ссылку (для мобильной версии)
+  $('.navbar-nav a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Обработка клика по всем ссылкам навигационного меню
+  $('#navbar-id a.nav-link').on('click', function(e) {
+    // Проверяем, является ли ссылка якорем
+    if(this.hash !== "") {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      // Вычисляем высоту навигационного бара
+      var fixed_offset = $('#navbar-id').outerHeight();
+
+      // Плавная прокрутка к элементу
+      $('html, body').stop().animate({
+        scrollTop: $target.offset().top - fixed_offset
+      }, 900, 'swing', function() {
+        window.location.hash = target;
+      });
+    }
+  });
+});
